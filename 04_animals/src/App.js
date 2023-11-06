a66f6d3e6519413b16b31cc7ce1ffc3f542d020b
sheng <a282104@gmail.com>
2023-11-06 20:00:05 +0800

    feat: Update AnimalShow component and add random animals to App
    This commit includes the following changes:
    - Update AnimalShow component to accept a type prop.
    - Use useState to manage an array of animals.
    - Implement a button to add random animals and display them.

--- 04_animals/src/App.js
+++ 04_animals/src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["dog", "cat", "bird"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    return (<div>
-        Show animal list here
-    </div>)
-};
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add animal</button>
+      <div>{renderAnimals}</div>
+    </div>
+  );
+}
 
 export default App;
